d0e2a44c85f05687739bffc71d9185b0df0df5ae
Tomas <tomasg92@gmail.com>
2021-06-25 16:49:36 -0400

    cambiado a UpperCamelCase

--- lib/employer/domain/employer.dart
+++ lib/employer/domain/employer.dart
@@ -1,4 +1,4 @@
-import 'package:citrus_app_mobile/user/domain/user.dart';
+import 'package:citrus_app_mobile/user/domain/User.dart';
 
 class Employer extends User {
   Employer(name, gender, birthDate) : super(name, gender, birthDate);
